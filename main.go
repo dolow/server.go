1c5ec5b3c57c9c4ca17970bb6ee86510eda8cf50
dolow <velvet.nanashi@gmail.com>
2020-10-12 22:29:40 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type Handler struct {
+	DocumentRoot string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s%s", h.DocumentRoot, r.URL.Path))
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write(data)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: main <port> <absolute document root>")
+		return
+	}
+
+	port    := os.Args[1]
+        docRoot := os.Args[2]
+
+	handler := &Handler{
+		DocumentRoot: docRoot,
+	}
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+		Handler: handler,
+	}
+
+	log.Printf("launch server on port %s\n", port)
+
+	go func(){
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, syscall.SIGTERM, os.Interrupt)
+		<-sigint
+
+		server.Shutdown(context.Background())
+
+
+		log.Println("server shutdown")
+	}()
+
+	server.ListenAndServe()
+
+	log.Println("finishing...")
+}
